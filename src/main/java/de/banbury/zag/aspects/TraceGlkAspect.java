d547c072388aab3d17945ce34a20c410d58363ed
Banbury <banbury20@gmail.com>
2014-06-11 18:49:42 +0200

    Added trace logging with AspectJ.

---
+++ src/main/java/de/banbury/zag/aspects/TraceGlkAspect.java
@@ -0,0 +1,27 @@
+package de.banbury.zag.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Aspect
+public class TraceGlkAspect {
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Before("execution(* org.p2c2e.zing.IGlk.*(..)) && !execution(void org.p2c2e.zing.swing.Glk.progress(String, int, int, int))")
+	public void logGlkCalls(JoinPoint jp) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(jp.getSignature());
+		sb.append(": ");
+		for (Object o : jp.getArgs()) {
+			if (o != null)
+				sb.append(o.toString());
+			else
+				sb.append("null");
+		}
+
+		log.trace(sb.toString());
+	}
+}
